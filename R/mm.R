c94b93b648dc3e316af830d0e9516a1426e6b41d
Nate Mercaldo <nate.mercaldo@vanderbilt.edu>
2017-04-12 18:34:15 -0500

    Removed reference to cordlike and AdaptiveQuad

--- R/mm.R
+++ R/mm.R
@@ -50,7 +50,7 @@ mm <- function(mean.formula, lv.formula = NULL, t.formula = NULL, id, data, init
   names(out$beta) = nms$beta
   names(out$alpha) = nms$alpha
   colnames(out$mod.cov) = rownames(out$mod.cov) = colnames(out$emp.cov) = rownames(out$emp.cov) = unlist(nms)
-  out$control = with(mm.fit, c(condlike, AdaptiveQuad, code, niter, length(table(id)), max(table(id))))
+  out$control = with(mm.fit, c(code, niter, length(table(id)), max(table(id))))
   
   aic = function(l=mm.fit$logL,k=nrow(mm.fit$modelcov)) 2*k-2*l
   bic = function(l=mm.fit$logL,k=nrow(mm.fit$modelcov),n=length(table(id))) -2*l + k *log(n) 
